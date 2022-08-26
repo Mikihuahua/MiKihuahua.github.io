2548df2c367694e1a36f135e0d0c85636f45d8cc
huahua <1543275321@qq.com>
2022-08-26 15:57:54 +0800

    first commit

---
+++ eventloop.js
@@ -0,0 +1,22 @@
+setTimeout(() => {
+    console.log("4");
+    setTimeout(() => {
+      console.log("8");
+    }, 0);
+    new Promise((r) => {
+      console.log("5");//构造函数是同步的
+      r();
+    }).then(() => {
+      console.log("7");//then()是异步的，这里已经入队
+    });
+    console.log("6");
+  }, 0);
+
+  new Promise((r) => {
+    console.log("1");//构造函数是同步的
+    r();
+  }).then(() => {
+    console.log("3");//then()是异步的，这里已经入队
+  });
+  console.log("2");
+//输出顺序：1 2 3 4 5 6 7 8 
